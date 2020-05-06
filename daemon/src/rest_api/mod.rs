1fba6808e091ddf66d2c83f11e61f82d9f3d5797
Rover <rover@RoverdeMacBook-Pro.local>
2020-05-06 08:00:45 +0800

    .service(web::resource("/agent").route(web::post().to(create_agent)))

--- daemon/src/rest_api/mod.rs
+++ daemon/src/rest_api/mod.rs
@@ -138,8 +138,8 @@ pub fn run(
                             .name("batch_statuses")
                             .route(web::get().to(get_batch_statuses)),
                     )
-                    .service(web::resource("/agent").route(web::post().to(do_create_agent)))
-                    .service(web::resource("/agent").route(web::put().to(do_update_agent)))
+                    .service(web::resource("/agent").route(web::post().to(create_agent)))
+                    .service(web::resource("/agent").route(web::put().to(update_agent)))
                     .service(
                         web::scope("/agent")
                             //.service(web::resource("").route(web::post().to(do_create_agent)))
